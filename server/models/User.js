adf939601e0638eaadd2aa455e66a0d65681bddd
Fayven <fayven.amelga@gmail.com>
2023-03-10 13:35:00 -0500

    Add user model

--- server/models/User.js
+++ server/models/User.js
@@ -1,8 +1,34 @@
 const { Schema } = require("mongoose");
 const bcrypt = require("bcrypt");
+const deck = require("./Deck");
 
 // TODO: 
-const userSchema = new Schema({});
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, 'Must match an email address!'],
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxLength: 14,
+  },
+  deck: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Deck',
+    },
+  ],
+});
 
 userSchema.pre("save", async function (next) {
   if (this.isNew || this.isModified("password")) {
