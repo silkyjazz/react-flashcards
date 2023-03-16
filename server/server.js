90c0af76cd7ba5f297636a51c438203925adf586
byxz885@gmail.com <53200767+byzESC@users.noreply.github.com>
2023-03-16 09:40:53 -0700

    deployement

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const { ApolloServer } = require('apollo-server-express');
 const db = require('./config/connection');
 
-const { authMiddleware } = require('./utils/auth');
+const { authMiddleware } = require('./utils/Auth');
 const { typeDefs, resolvers } = require('./schemas');
 
 const PORT = process.env.PORT || 3001;
